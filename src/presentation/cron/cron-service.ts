2f4b53c5adc9865ffc67c80ea03e1b4d282573ed
Lautaro <lautarodm98@gmail.com>
2024-05-17 23:56:20 -0300

    fin seccion 8

---
+++ src/presentation/cron/cron-service.ts
@@ -0,0 +1,17 @@
+import { CronJob } from "cron";
+
+// fernando usa interfaces cuando manejamos objetos con propiedaedes
+// cuando queremos crear un tipo de dato usamos types
+type CronTime = string | Date;
+type OnTick = () => void
+
+export class CronService {
+
+  static createJob(cronTime: CronTime, onTick: OnTick): CronJob {
+    const job = new CronJob(cronTime, onTick);
+
+    job.start();
+
+    return job
+  }
+}
